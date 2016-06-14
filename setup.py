5ac8b9b717ed6f44cf8874b181e6458d0e8d5c5f
Ubuntu <dengyuntian@gmail.com>
2016-06-14 22:06:06 +0000

    copy pub key

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import sys, argparse, logging, subprocess
+import sys, argparse, logging, subprocess, os
 
 from utils import parse_params
 
@@ -6,7 +6,7 @@ def process_args(args):
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--name', dest="name",
-                        type=str,
+                        type=str, required=True,
                         help=('Name of the machine. You can use any nickname '
                             'as long as it is not used by other machines.' 
                         ))
@@ -28,12 +28,25 @@ def main(args):
         format='%(asctime)-15s %(name)-5s %(levelname)-8s %(message)s',
         filename=parameters.log_path)
 
+    home_dir = os.path.expanduser('~')
+    app_dir = os.path.dirname(os.path.realpath(__file__))
+    app_relative_dir = app_dir.replace(home_dir, '~')
+    logging.info('Application base directory: %s.' %app_relative_dir)
+    base_ssh_dir = '~/.ssh'
+    name = parameters.name
+    base_ssh_dir_abs = base_ssh_dir.replace('~',os.path.expanduser('~'))
+    name = parameters.name
     # read config file
     params = parse_params(parameters.config_path)
     # add public key file to central server public key file
     assert 'central' in params, 'central server must be set in config file %s. The required format is \'central: username@machine-ip\''%parameters.config_path
-    logging.info('Central server: %s'%params['central'])
-    subprocess.call("exit 1", shell=True)
+
+    central_server = params['central']
+    logging.info('Central server: %s'%central_server)
+    source_pub_key = os.path.join(base_ssh_dir_abs, 'id_rsa.pub')
+    dest_pub_key = "%s:%s"%(central_server,os.path.join(base_ssh_dir, 'id_rsa.pub.%s'%name))
+    print ('scp %s %s'%(source_pub_key, dest_pub_key))
+    ret = subprocess.call(["scp", source_pub_key, dest_pub_key])
 
 if __name__ == "__main__":
     main(sys.argv[1:])
